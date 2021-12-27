fcd1d8331a5a4a797d37aa2700393320058cd942
Shin <struct.developer@gmail.com>
2021-12-27 22:15:50 +0900

    [HackerRank] Mini-Max Sum

---
+++ HackerRank/Mini-Max Sum.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func miniMaxSum(arr: [Int]) -> Void {
+    let min = arr.sorted()[0..<4].reduce(0, +)
+    let max = arr.sorted()[arr.count-4..<arr.count].reduce(0, +)
+    print("\(min) \(max)")
+}
+
+guard let arrTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let arr: [Int] = arrTemp.split(separator: " ").map {
+    if let arrItem = Int($0) {
+        return arrItem
+    } else { fatalError("Bad input") }
+}
+
+guard arr.count == 5 else { fatalError("Bad input") }
+
+miniMaxSum(arr: arr)
