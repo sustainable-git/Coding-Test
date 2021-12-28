de3955196b096e458c6c17613ddba07be9e6ac0f
Shin <struct.developer@gmail.com>
2021-12-28 22:53:13 +0900

    [HackerRank] Lonely Integer

---
+++ HackerRank/Lonely Integer.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+func lonelyinteger(a: [Int]) -> Int {
+    var numbers: [Int] = a
+    var result: Int = numbers.removeFirst()
+    numbers.forEach {
+        result = result ^ $0
+    }
+
+    return result
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let aTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let a: [Int] = aTemp.split(separator: " ").map {
+    if let aItem = Int($0) {
+        return aItem
+    } else { fatalError("Bad input") }
+}
+
+guard a.count == n else { fatalError("Bad input") }
+
+let result = lonelyinteger(a: a)
+
+fileHandle.write(String(result).data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
