1418d9c6a4d05272b8f23868892955543299bf7e
Shin <struct.developer@gmail.com>
2021-12-29 11:06:27 +0900

    [HackerRank] Counting Sort 1

---
+++ HackerRank/Counting Sort 1.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func countingSort(arr: [Int]) -> [Int] {
+    var array: [Int] = Array(repeating: 0, count: 100)
+    arr.forEach {
+        array[$0] += 1
+    }
+
+    return array
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let arrTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let arr: [Int] = arrTemp.split(separator: " ").map {
+    if let arrItem = Int($0) {
+        return arrItem
+    } else { fatalError("Bad input") }
+}
+
+guard arr.count == n else { fatalError("Bad input") }
+
+let result = countingSort(arr: arr)
+
+fileHandle.write(result.map{ String($0) }.joined(separator: " ").data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
