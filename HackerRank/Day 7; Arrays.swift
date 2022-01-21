1d1740d6bf7e278674aab443cc4294ef50a0d3b4
Shin <struct.developer@gmail.com>
2022-01-21 11:14:27 +0900

    [HackerRank] Day 7: Arrays

---
+++ HackerRank/Day 7; Arrays.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let arrTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let arr: [Int] = arrTemp.split(separator: " ").map {
+    if let arrItem = Int($0) {
+        return arrItem
+    } else { fatalError("Bad input") }
+}
+
+guard arr.count == n else { fatalError("Bad input") }
+
+arr.reversed().forEach {
+    print($0, terminator: " ")
+}
