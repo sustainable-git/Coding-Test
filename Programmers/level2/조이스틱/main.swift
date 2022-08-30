eaa27c06a1ff9a5af7d3a521d1e010136b46966e
sustainable-git <struct.developer@gmail.com>
2022-08-31 00:19:38 +0900

    [Programmers] 조이스틱

--- Programmers/level2/조이스틱/main.swift
+++ Programmers/level2/조이스틱/main.swift
@@ -1,24 +1,59 @@
 import Foundation
 
 func solution(_ name:String) -> Int {
-    var nCopyArr = name.map{String($0)}
-    var upDownCount = 0
-    var leftRightCount = name.count - 1 // right now, it is the biggest
+    guard name != String(repeating: "A", count: name.count) else  { return 0 }
+    var minCount: Int = Int.max
     
-    var dictionary = [String : Int]()
-    for i in 65..<91 { // dictionary initializer
-        let value = i - 65
-        dictionary[String(UnicodeScalar(UInt8(i)))] = value < 26 - value ? value : 26 - value
+    func btFront(head: String.Index, current: String, count: Int, turn: Bool) {
+        if count > minCount { return }
+        var newCount = count
+        var newCurrent = current
+        if current[head] != name[head] {
+            newCurrent.remove(at: head)
+            newCurrent.insert(name[head], at: head)
+            newCount += numberOfJoystickMoves(of: name[head])
+            if newCurrent == name { minCount = min(minCount, newCount); return }
+        }
+        btFront(head: name.circularIndex(after: head), current: newCurrent, count: newCount + 1, turn: turn)
+        if !turn {
+            btBackward(head: name.circularIndex(before: head), current: newCurrent, count: newCount + 1, turn: true)
+        }
     }
-    for alphabet in nCopyArr { upDownCount += dictionary[alphabet]! } // upDownCount initializer
     
-    for index in 0..<name.count { // to check if we can get better option at each indexs
-        var isAIndex = index + 1
-        while isAIndex < name.count && nCopyArr[isAIndex] == "A" { isAIndex += 1 }
-        let leftRightMoves = 2 * index + name.count - isAIndex
-        
-        leftRightCount = min(leftRightCount, leftRightMoves)
+    func btBackward(head: String.Index, current: String, count: Int, turn: Bool) {
+        if count > minCount { return }
+        var newCount = count
+        var newCurrent = current
+        if current[head] != name[head] {
+            newCurrent.remove(at: head)
+            newCurrent.insert(name[head], at: head)
+            newCount += numberOfJoystickMoves(of: name[head])
+            if newCurrent == name { minCount = min(minCount, newCount); return }
+        }
+        btBackward(head: name.circularIndex(before: head), current: newCurrent, count: newCount + 1, turn: turn)
+        if !turn {
+            btFront(head: name.circularIndex(after: head), current: newCurrent, count: newCount + 1, turn: true)
+        }
+    }
+    btFront(head: name.startIndex, current: String(repeating: "A", count: name.count), count: 0, turn: false)
+    btBackward(head: name.startIndex, current: String(repeating: "A", count: name.count), count: 0, turn: false)
+    
+    return minCount
+}
+
+func numberOfJoystickMoves(of char: Character) -> Int {
+    let alphabet: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let up = alphabet.distance(from: alphabet.startIndex, to: alphabet.firstIndex(of: char)!)
+    let down = abs(alphabet.distance(from: alphabet.endIndex, to: alphabet.firstIndex(of: char)!))
+    return min(up, down)
+}
+
+extension String {
+    func circularIndex(after index: String.Index) -> String.Index {
+        return index == self.index(before: endIndex) ? self.startIndex : self.index(after: index)
     }
     
-    return upDownCount + leftRightCount
+    func circularIndex(before index: String.Index) -> String.Index {
+        return index == self.startIndex ? self.index(before: endIndex) : self.index(before: index)
+    }
 }
